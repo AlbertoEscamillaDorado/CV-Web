9ad325f8ccf0adbcae65aa15757417294f9678bf
AlbertoEscamillaDorado <alesdo2003@gmail.com>
2026-02-19 18:10:07 +0100

    Añadido github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://AlbertoEscamillaDorado.github.io',
+    base: '/CV-Web/',
+});
